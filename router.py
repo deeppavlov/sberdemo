a4d72353f1fe1c06462c6a14195a613495a6e0c3
yoptar <yoptar@gmail.com>
2017-09-07 15:11:19 +0300

    refactor: moved basic nlu from router to a new file

--- router.py
+++ router.py
@@ -4,11 +4,11 @@ import copy
 import time
 
 from .nlu import *
+from .graph_based_nlu import GraphBasedSberdemoNLU
+from .slots import *
 
 from telegram.ext import Updater
 from telegram.ext import CommandHandler, MessageHandler, Filters
-
-from .slots import *
 
 
 def format_route(route):
@@ -32,68 +32,6 @@ def parse_route(file_name):
         format_route(route)
 
     return data
-
-
-def some_starts_with(words_list, *prefixi_to_check):
-    for w in words_list:
-        if w.startswith(prefixi_to_check):
-            return True
-    return False
-
-
-class GraphBasedSberdemoNLU:
-
-    def __init__(self):
-        self.expect = None
-
-    def set_expectation(self, expect):
-        self.expect = expect
-
-    def forward(self, embedding, text: List[Dict]):
-        res = {'slots': {}}
-
-        normals = {w['normal'] for w in text}
-        intent = None
-
-        slot_values = {NEW_ACC_CURRENCY: None,
-                       'slot_address': None,
-                       'see_docs': None}
-
-        if {'да', 'давай', 'давайте', 'конечно', 'хорошо', 'ладно', 'ок', 'ok'}.intersection(set(w['_text'] for w in text)):
-            res['confirmation'] = True
-        if {'нет'}.intersection(set(w['_text'] for w in text)):
-            res['confirmation'] = False
-
-        if self.expect and 'confirmation' in res:
-            res['slots'][self.expect] = res['confirmation']
-            del res['confirmation']
-
-        if {'открыть', 'счет'}.issubset(normals):
-            intent = 'new_acc_intent'
-
-        words = [w['_text'] for w in text]
-        if some_starts_with(words, 'рубл', 'rub'):
-            slot_values[NEW_ACC_CURRENCY] = 'RUB'
-        elif some_starts_with(words, 'евро', 'EUR'):
-            slot_values[NEW_ACC_CURRENCY] = 'EUR'
-        elif some_starts_with(words, 'доллар', 'долар', 'бакс', 'usd', '$', 'dollar'):
-            slot_values[NEW_ACC_CURRENCY] = 'USD'
-
-        if some_starts_with(words, 'документ'):
-            slot_values[NEW_ACC_SHOW_DOCS] = True
-
-        if some_starts_with(words, 'ooo', 'ооо'):
-            slot_values[NEW_ACC_OWNERSHIP_FORM] = 'ооо'
-        if some_starts_with(words, 'зао'):
-            slot_values[NEW_ACC_OWNERSHIP_FORM] = 'зао'
-
-        if intent is not None:
-            res['intent'] = intent
-        for k, v in slot_values.items():
-            if v:
-                res['slots'][k] = v
-
-        return res
 
 
 class Dialog:
