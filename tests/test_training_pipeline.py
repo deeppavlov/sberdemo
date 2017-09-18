accde6b56a647da26486cab249ecd0323137a39d
Marat Zaynutdinov <tsundokum@gmail.com>
2017-09-18 12:39:49 +0300

    test: fix

--- tests/test_training_pipeline.py
+++ tests/test_training_pipeline.py
@@ -14,42 +14,44 @@ def run(*args):
     print('Run: ', *args)
     return subprocess.run(args, stderr=PIPE, stdout=PIPE, check=True)
 
-assert 'TOMITA_PATH' in os.environ, 'Please specify path to Tomita Parser in $TOMITA_PATH'
 
-dir = tempfile.TemporaryDirectory(prefix='sberdemotest_').name
-os.mkdir(dir)
+if __name__ == '__main__':
+    assert 'TOMITA_PATH' in os.environ, 'Please specify path to Tomita Parser in $TOMITA_PATH'
 
-dataset = os.path.join(dir, 'dataset.tsv')
-templates = 'generative_templates.tsv'
-slot_definitions = 'slots_definitions.tsv'
+    dir = tempfile.TemporaryDirectory(prefix='sberdemotest_').name
+    os.mkdir(dir)
 
-assert os.path.isfile('generate_from_templates.py'), '"generate_from_templates.py" not found'
+    dataset = os.path.join(dir, 'dataset.tsv')
+    templates = 'generative_templates.tsv'
+    slot_definitions = 'slots_definitions.tsv'
 
-run('python3', 'generate_from_templates.py', '--output', dataset, '--templates', templates)
+    assert os.path.isfile('generate_from_templates.py'), '"generate_from_templates.py" not found'
 
-with open(dataset) as f:
-    for line in f:
-        assert '#' not in line.strip(), 'Found "#" in line "{}"'.format(line)
-print('Dataset successfully created in "{}"'.format(dataset))
+    run('python3', 'generate_from_templates.py', '--output', dataset, '--templates', templates)
+
+    with open(dataset) as f:
+        for line in f:
+            assert '#' not in line.strip(), 'Found "#" in line "{}"'.format(line)
+    print('Dataset successfully created in "{}"'.format(dataset))
 
 
-run('python3', 'train_svm.py', '--folder', dir, '--data', dataset, '--slot_path', slot_definitions, '--slot_train')
-print('slots trained')
-run('python3', 'train_svm.py', '--folder', dir, '--data', dataset, '--slot_path', slot_definitions, '--intent_train')
-print('intent recognizer trained')
+    run('python3', 'train_svm.py', '--folder', dir, '--data', dataset, '--slot_path', slot_definitions, '--slot_train')
+    print('slots trained')
+    run('python3', 'train_svm.py', '--folder', dir, '--data', dataset, '--slot_path', slot_definitions, '--intent_train')
+    print('intent recognizer trained')
 
-pipe = nlu.create_pipe()
-slots.read_slots_serialized(dir, pipe)
-print('Slots deserialized')
+    pipe = nlu.create_pipe()
+    slots.read_slots_serialized(dir, pipe)
+    print('Slots deserialized')
 
-print()
+    print()
 
-print('The test has just passed!')
+    print('The test has just passed!')
 
-print()
+    print()
 
-print('cleaning up...')
-shutil.rmtree(dir)
-print('done')
+    print('cleaning up...')
+    shutil.rmtree(dir)
+    print('done')
 
 
