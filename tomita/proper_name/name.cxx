67ae2b49050a388d91835424ee673500de50451c
yoptar <yoptar@gmail.com>
2017-09-27 12:02:41 +0300

    fix: only expect name in singular

--- tomita/proper_name/name.cxx
+++ tomita/proper_name/name.cxx
@@ -1,8 +1,8 @@
 #encoding "utf8"
 #GRAMMAR_ROOT ProperName
 
-Name -> Word<gnc-agr[1],gram="имя,им",rt> interp (ProperName.First) (Word<gnc-agr[1],gram="отч"> interp (ProperName.Middle));
-Name -> Word<gnc-agr[1],gram="имя,твор",rt> interp (ProperName.First) (Word<gnc-agr[1],gram="отч"> interp (ProperName.Middle));
+Name -> Word<gnc-agr[1],gram="имя,им,ед",rt> interp (ProperName.First) (Word<gnc-agr[1],gram="отч"> interp (ProperName.Middle));
+Name -> Word<gnc-agr[1],gram="имя,твор,ед",rt> interp (ProperName.First) (Word<gnc-agr[1],gram="отч"> interp (ProperName.Middle));
 LastName -> Word<gram="фам",rt> interp (ProperName.Last);
 
 ProperName -> (LastName<gnc-agr[1]>) Name<gnc-agr[1]>;
