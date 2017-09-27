0ac083e8b1571923f84036816dc9bbd6d9b92208
yoptar <yoptar@gmail.com>
2017-09-27 10:53:43 +0300

    fix: correct parsing of names in feminine gender

--- tomita/proper_name/name.cxx
+++ tomita/proper_name/name.cxx
@@ -1,7 +1,8 @@
 #encoding "utf8"
 #GRAMMAR_ROOT ProperName
 
-Name -> Word<gnc-agr[1],gram="имя"> interp (ProperName.First) (Word<gnc-agr[1],gram="отч"> interp (ProperName.Middle));
+Name -> Word<gnc-agr[1],gram="имя,им"> interp (ProperName.First) (Word<gnc-agr[1],gram="отч"> interp (ProperName.Middle));
+Name -> Word<gnc-agr[1],gram="имя,твор"> interp (ProperName.First) (Word<gnc-agr[1],gram="отч"> interp (ProperName.Middle));
 LastName -> Word<gram="фам"> interp (ProperName.Last);
 
 ProperName -> (LastName) Name;
