d7c5286ef30031ae83e37ae83d1a7460eeae8814
yoptar <yoptar@gmail.com>
2017-09-27 15:26:03 +0300

    feat: implemented new faq request api

--- services.py
+++ services.py
@@ -5,16 +5,12 @@ from urllib.parse import urlencode
 
 def faq(text: str, threshold=0.95):
     params = {
-        "qna_id": "SBRF",
         "q": text
     }
-    url = 'http://lnsigo.dc.phystech.edu:5001/answer?' + urlencode(params)
-    _, response = json.loads(urllib.request.urlopen(url).read().decode('UTF8'))['answers'][0]
-    del response['avg'], response['score']
-    response['top'] = round(response['top'], 2)
-    for q in response['questions']:
-        q['s'] = round(q['s'], 2)
-    answer = response['answer'] if response['top'] >= threshold else None
+    url = 'http://lnsigo.dc.phystech.edu:5000/answer?' + urlencode(params)
+    response = json.loads(urllib.request.urlopen(url).read().decode('UTF8'))
+    response['score'] = round(response['score'], 2)
+    answer = response['answer'] if response['score'] >= threshold else None
     return answer, response
 
 
