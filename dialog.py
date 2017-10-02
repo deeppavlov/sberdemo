c76c88a68653ec1c937ba07004e8d9fa923d342d
yoptar <yoptar@gmail.com>
2017-10-02 18:43:48 +0300

    minor: show chit-chat messages in italic

--- dialog.py
+++ dialog.py
@@ -105,7 +105,7 @@ class Dialog:
                         response = response[:name['pos']] + response[name['pos']+name['len']:]
                 response = response.strip(' \t\n\r\x0b\x0c,')
                 response = response[0].upper() + response[1:]
-                response = ["CHIT-CHAT\n" + html.escape(response)]
+                response = ["<i>{}</i>".format(html.escape(response))]
             except Exception as e:
                 self.logger.error(e)
                 response = ['CHIT-CHAT ERROR: {}'.format(e)]
